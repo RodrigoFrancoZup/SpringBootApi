d8e0e1b0fbbd5b32f2d1851c3ddc0a074a2f1c73
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-04-17 16:39:14 -0300

    Realizando busca personalizada por JPQL e pelo Padrao de Nomes do Spring

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,27 +3,50 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.modelo.dto.TopicoDTO;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
-	
+
+	// Criando objeto de TopicoRepository
+	// @Autowired da new no objeto para gente
+	@Autowired
+	TopicoRepository topicoRepositorioRepository;
+
 	@GetMapping("/topicos")
-	public List<TopicoDTO> lita(){
-		//Criando um topico
-		Topico topico = new Topico("Dúvida XPTO", "Como fazer tal coisa?", new Curso("Java", "Programacao"));
-		
-		/*Colocando em uma lista o mesmo tópico 3x
-		Arrays.asList(topico, topico, topico);
+	public List<TopicoDTO> lita() {
+		// Criando um topico
+		// Topico topico = new Topico("Dúvida XPTO", "Como fazer tal coisa?", new
+		// Curso("Java", "Programacao"));
+
+		/*
+		 * Colocando em uma lista o mesmo tópico 3x Arrays.asList(topico, topico,
+		 * topico);
+		 */
+
+		// Converte lista de topico para lista de topicoDTO e retorna
+
+		return TopicoDTO.converter(topicoRepositorioRepository.findAll());
+	}
+
+	@GetMapping("/topicosPorCurso")
+	public List<TopicoDTO> buscaTopicoPorNomeDeCurso(String nomeCurso) {
+
+		/* BUSCANDO PELA JPQL
+		List<Topico> topicos = topicoRepositorioRepository.buscaTopicoPorNomeDeCurso(nomeCurso);
 		*/
 		
-		//Converte lista de topico para lista de topicoDTO e retorna
-		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+		List<Topico> topicos = topicoRepositorioRepository.findByCursoNome(nomeCurso);
+		List<TopicoDTO> topicosDTO = TopicoDTO.converter(topicos);
+
+		return topicosDTO;
 	}
 
 }
---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,22 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	/*
+	 * Busca por JPQL
+	 * 
+	 * @Query("SELECT t from Topico t WHERE t.curso.nome = :nomeCurso") 
+	 * public List<Topico> buscaTopicoPorNomeDeCurso (@Param("nomeCurso")String nome);
+	 */
+
+	public List<Topico> findByCursoNome(String nome);
+
+}
